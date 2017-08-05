a7fb09ff5a35a4f6fb753e2c1c5551c6cda0f29f
覃永利 <1538306377@qq.com>
2017-08-05 10:39:54 +0800

    fix bug

--- main.js
+++ main.js
@@ -83,7 +83,7 @@
 
 let images = [];
 for(let i = 1; i <= 8; i++){
-	images.push(`images/${i}.JPG`);
+	images.push(`./images/${i}.JPG`);
 }
 let slider1 = new Slider({
 	container: document.querySelector('#container'),
